aaea5fc818e2e19457354d9054546e9d8f5188f4
null0routed <81880083+null0routed@users.noreply.github.com>
2025-09-13 19:13:39 -0400

    Added player rendering and rotation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 def main():
     # print("Starting Asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
@@ -7,14 +9,20 @@ def main():
     (pygame_init_mod, pygame_fail_mod) = pygame.init()
     print(f"Init: {pygame_init_mod}\nFailed: {pygame_fail_mod}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
